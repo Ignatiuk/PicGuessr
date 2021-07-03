f3a24e89e5daf7227b5ebf01ad18ea3fa60e11f7
Alexander <54143543+merikovich@users.noreply.github.com>
2021-07-03 05:59:38 +0300

    feat(utils/utils): shuffleArray function

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,5 +1,15 @@
 export function getRandomNumber(min, max) {
-  const rand = min - 0.5 + Math.random() * (max - min + 1);
+  const rand = min - 0.5 + Math.random() * (max - min + 1)
 
-  return Math.round(rand);
+  return Math.round(rand)
+}
+
+export function shuffleArray(array) {
+  // from https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
+  // it's https://en.wikipedia.org/wiki/Schwartzian_transform
+
+  return array
+    .map((a) => ({ sort: Math.random(), value: a }))
+    .sort((a, b) => a.sort - b.sort)
+    .map((a) => a.value)
 }
