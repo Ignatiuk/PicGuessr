1c77c551ac42bae8fe20a6ac837f87137da8ad95
Alexander <54143543+merikovich@users.noreply.github.com>
2021-07-03 05:59:01 +0300

    feat(main.js): global components & plugins

--- src/main.js
+++ src/main.js
@@ -1,12 +1,22 @@
-import Vue from 'vue';
-import App from './App.vue';
-import router from './router';
-import store from './store';
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
 
-Vue.config.productionTip = false;
+import axios from '@/plugins/axios'
+
+import BaseButton from '@/components/BaseButton/BaseButton'
+import BaseInput from '@/components/BaseInput/BaseInput'
+import BaseLoader from '@/components/BaseLoader/BaseLoader'
+
+Vue.component('BaseButton', BaseButton)
+Vue.component('BaseInput', BaseInput)
+Vue.component('BaseLoader', BaseLoader)
+
+Vue.prototype.$axios = axios
+
+Vue.config.productionTip = false
 
 new Vue({
   router,
-  store,
   render: (h) => h(App),
-}).$mount('#app');
+}).$mount('#app')
